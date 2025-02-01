dbc6731e66fc2864bbfbce25028a366014aa55f0
Yi Hein Chai <chaiyihein@gmail.com>
2025-02-01 19:49:44 +0000

    build topo graph, add ReLU, add sub, add rtruediv, addmul

--- engine.py
+++ engine.py
@@ -41,70 +41,98 @@ class Value:
         return Value(new_value, new_tree)
 
     def __add__(self, other: "Value"):
-        return self._apply_operation(other, lambda x, y: x + y, "add")
+        return self._apply_operation(other, lambda x, y: x + y, "+")
 
     def __radd__(self, other: "Value"):
-        return self._apply_operation(other, lambda x, y: x + y, "add")
+        return self._apply_operation(other, lambda x, y: x + y, "+")
 
     def __iadd__(self, other: "Value"):
-        return self._apply_operation(other, lambda x, y: x + y, "add")
+        return self._apply_operation(other, lambda x, y: x + y, "+")
 
     def __sub__(self, other: "Value"):
-        return self._apply_operation(other, lambda x, y: x - y, "sub")
+        return self._apply_operation(other, lambda x, y: x - y, "-")
 
     def __mul__(self, other: "Value"):
-        return self._apply_operation(other, lambda x, y: x * y, "mul")
+        return self._apply_operation(other, lambda x, y: x * y, "*")
 
     def __pow__(self, other: "Value"):
-        return self._apply_operation(other, lambda x, y: x**y, "pow")
+        return self._apply_operation(other, lambda x, y: x**y, "**")
 
     def __truediv__(self, other: "Value"):
         return self * (other**-1)
 
+    def __rtruediv__(self, other: "Value"):
+        return other * (self**-1)
+
     def __neg__(self):
         return self * -1
 
-    def backward(self):
-        self._backward(root=True)
+    def __rmul__(self, other):
+        return self * other
 
-    def _backward(self, root=False):
+    def relu(self):
+        return self._apply_operation(None, lambda x, _: 0 if x < 0 else x, "ReLU")
+
+    def backward(self):
         if not isinstance(self.tree, Op):
             raise ValueError("Operation tree is corrupted.")
 
-        operand1 = self.tree.operand1
-        operand2 = self.tree.operand2
+        self.grad = 1
 
-        if root:
-            self.grad = 1
+        topo_order = []
+        visited = set()
 
-        method = self.tree.method
-        if method in ["add", "sub"]:
-            operand1.grad += self.grad * 1
-            operand2.grad += self.grad * 1
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                if isinstance(v.tree, Op) and not v.tree.method == "value":
+                    if v.tree.operand1:
+                        build_topo(v.tree.operand1)
+                    if v.tree.operand2:
+                        build_topo(v.tree.operand2)
+                topo_order.append(v)
 
-            operand1._backward()
-            operand2._backward()
+        build_topo(self)
 
-        if method == "pow":
-            operand1.grad += (
-                self.grad * operand2.value * (operand1.value ** (operand2.value - 1))
-            )
-            operand2.grad += (
-                self.grad * operand1.value**operand2.value * math.log(operand1.value)
-            )
+        for v in reversed(topo_order):
+            if not isinstance(v.tree, Op):
+                continue
 
-            operand1._backward()
-            operand2._backward()
+            operand1 = v.tree.operand1
+            operand2 = v.tree.operand2
+            method = v.tree.method
 
-        if method in ["mul"]:
-            operand1.grad += self.grad * operand2.value
-            operand2.grad += self.grad * operand1.value
+            if method == "+":
+                operand1.grad += v.grad * 1
+                operand2.grad += v.grad * 1
 
-            operand1._backward()
-            operand2._backward()
+            if method == "-":
+                operand1.grad += v.grad * 1
+                operand2.grad += v.grad * -1
 
-        if method == "value":
-            pass
+            if method == "**":
+                operand1.grad += (
+                    v.grad * operand2.value * (operand1.value ** (operand2.value - 1))
+                )
+
+                try:
+                    operand2.grad += (
+                        v.grad
+                        * operand1.value**operand2.value
+                        * math.log(operand1.value)
+                    )
+                except ValueError:
+                    pass
+
+            if method in ["*"]:
+                operand1.grad += v.grad * operand2.value
+                operand2.grad += v.grad * operand1.value
+
+            if method == "ReLU":
+                operand1.grad += v.grad * (0 if operand1.value <= 0 else 1)
+
+            if method == "value":
+                pass
 
 
 #  If you have diverging paths, you just add the gradients together
